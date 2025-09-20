b373d3512dddc749f8a494409bd6296c00ab71c5
Milton Munoz <livemilton@gmail.com>
2025-09-20 06:33:22 -0500

    angular bootcamp: :sparkles: content projection with ng-content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, Input, Output, EventEmitter, output } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 // type User ={
 //   id: string;
@@ -12,7 +13,8 @@ import { type User } from './user.model';
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+  imports: [CardComponent]
 })
 export class UserComponent {
   @Input({ required:true }) user!: User;
