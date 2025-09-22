bae7290e6ac69d692971731849d48940678de4ec
Milton Munoz <livemilton@gmail.com>
2025-09-21 20:47:18 -0500

    angular bootcamp: :sparkles: Creating & Using Shared Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { DatePipe } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     NewTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, DatePipe, FormsModule],
+  imports: [BrowserModule, DatePipe, FormsModule, SharedModule],
 })
 export class AppModule {}
