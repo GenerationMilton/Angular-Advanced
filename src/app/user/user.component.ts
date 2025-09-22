1f315b082b4878fb3fb7da57df32a302d9c390c1
Milton Munoz <livemilton@gmail.com>
2025-09-21 19:59:57 -0500

    angular bootcamp: :sparkles: Migrating all components To use modules

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
 import { Component, computed, Input, Output, EventEmitter, output } from '@angular/core';
 import { type User } from './user.model';
-import { CardComponent } from "../shared/card/card.component";
 
 // type User ={
 //   id: string;
@@ -11,10 +10,9 @@ import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
-  standalone: true,
+  standalone: false,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
-  imports: [CardComponent]
 })
 export class UserComponent {
   @Input({ required:true }) user!: User;
